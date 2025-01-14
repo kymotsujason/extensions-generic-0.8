843c08387c8c45092c4dad6a467bd4bd0484aae0
Jason Yue <kymotsujason@gmail.com>
2025-01-13 22:53:02 -0800

    Madara: Disable next page loadng to prevent the spam on Toonily, this is not a fix, this is just a workaround for me. Here for those who also want the workaround: https://kymotsujason.github.io/extensions-generic-0.8/madara/

--- src/Madara.ts
+++ src/Madara.ts
@@ -25,7 +25,7 @@ import * as cheerio from 'cheerio'
 import { Parser } from './MadaraParser'
 import { URLBuilder } from './MadaraHelper'
 
-const BASE_VERSION = '3.2.1'
+const BASE_VERSION = '3.2.2'
 export const getExportVersion = (EXTENSION_VERSION: string): string => {
     return BASE_VERSION.split('.').map((x, index) => Number(x) + Number(EXTENSION_VERSION.split('.')[index])).join('.')
 }
@@ -354,7 +354,7 @@ export abstract class Madara implements SearchResultsProviding, MangaProviding, 
 
         return App.createPagedResults({
             results: manga,
-            metadata: { page: (page + 1) }
+            metadata: undefined
         })
     }
 
